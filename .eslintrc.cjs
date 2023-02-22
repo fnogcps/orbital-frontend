f438612c7515d6c06f648e044560ecbc3b2f8b26
Felipe Nogueira <contato.fnog@gmail.com>
2023-02-22 10:28:54 -0300

    Add typescript-eslint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,16 +2,24 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    node: true,
+    node: true
   },
-  extends: ["eslint:recommended", "plugin:vue/vue3-recommended"],
-  overrides: [],
+  parser: "vue-eslint-parser",
   parserOptions: {
+    parser: "@typescript-eslint/parser",
     ecmaVersion: "latest",
-    sourceType: "module",
+    sourceType: "module"
   },
-  plugins: ["vue"],
+  extends: [
+    "eslint:recommended",
+    "plugin:prettier/recommended",
+    "plugin:vue/vue3-essential",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended"
+  ],
+
+  plugins: ["vue", "prettier", "@typescript-eslint"],
   rules: {
-    "vue/multi-word-component-names": 0,
-  },
-}
+    "prettier/prettier": "warn"
+  }
+};
